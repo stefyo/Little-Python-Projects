303fb62204e52fbaa7e90063d0cec23443f0fa87
stefyo <118811235+stefyo@users.noreply.github.com>
2023-11-16 10:57:53 +0000

    Add files via upload

---
+++ SECURE LOGIN.py
@@ -0,0 +1,13 @@
+print("""
+MY LOGIN SYSTEM
++++++++++++++++""")
+name = input("Username > ")
+password = input("Password > ")
+if name == "Alouette" and password == "Un1c0rn":
+  print("Hello beautiful and wonderful and funny girl! I hope you have an incredible day!")
+elif name == "Artie" and password == "turtle":
+  print("Bubble double! Bubble double! Bubble double!")
+elif name == "Stef" and password == "5tef":
+  print("Hey Stef! Have you been keeping up with your 100 days of code? If not, why not?!")
+else:
+  print("Get the heck outta here! Go on, SCRAM!")
