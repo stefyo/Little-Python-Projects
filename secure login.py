b62e62b03f6e114e074e6d743c8182e0db3ce48b
stefyo <118811235+stefyo@users.noreply.github.com>
2023-12-08 14:21:29 +0000

    Update and rename SECURE LOGIN.py to secure login.py

--- secure login.py
+++ secure login.py
@@ -7,7 +7,7 @@ if name == "Alouette" and password == "Un1c0rn":
   print("Hello beautiful and wonderful and funny girl! I hope you have an incredible day!")
 elif name == "Artie" and password == "turtle":
   print("Bubble double! Bubble double! Bubble double!")
-elif name == "Stef" and password == "5tef":
-  print("Hey Stef! Have you been keeping up with your 100 days of code? If not, why not?!")
+elif name == "Stef" and password == "Stef":
+  print("Hey Stef! Ummm that's not a very safe password. And have you been keeping up with your 100 days of code? If not, why not?!")
 else:
   print("Get the heck outta here! Go on, SCRAM!")
